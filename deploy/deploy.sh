f751e244675af02ec2f12c18b15ceb6f6c83ff85
avuko <iamavuko@gmail.com>
2017-09-10 22:24:30 +0200

    STILL PREPROD: many changes related to performance

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,6 +3,26 @@ set -e
 set -u
 
 target="${1}"
+
+if [ "grep -q prep.sh \"AREYOUSURE='no'\"" ];then
+   echo ""
+   echo "You have not set the buildscript to set a"
+   echo "password for root on the randori system."
+   echo "If you are redeploying, this is not a problem."
+   echo "Otherwise, please change AREYOURSURE='no' to"
+   echo "AREYOUSURE='yes' in prep.sh"
+   echo ""
+   echo "Continue with setting a root password? (yes/no)"
+
+   read continueinstall
+
+   if [ "${continueinstall}" == 'yes' ]; then
+	echo "Continuing deployment"
+   else
+	exit
+   fi
+fi
+
 opensshdir='openssh-7.2p2'
 
 copy_over=(rsyslog prep.sh make.sh pam_randori.c common-auth startup.sh killall.sh\
@@ -13,9 +33,13 @@ for co in ${copy_over[@]}; do
  echo $co;
  if [ $co == 'common-auth' ]; then
 	scp $co ${target}:/etc/pam.d/
+elif [ $co == 'killall.sh' ]; then
+	scp $co ${target}:.
+	# run it to stop any previous installations
+	ssh ${target} './killall.sh'
 elif [ $co == 'rsyslog' ]; then
-	echo "!! WARNING: please note I'm setting your logfiles up to"
-	echo "pile up for a full year. This could fill up your disks."
+	echo '!! WARNING: please note I am setting your logfiles up to'
+	echo 'pile up for a full year. This could fill up your disks.'
 	scp $co ${target}:/etc/logrotate.d/rsyslog
 elif [ $co == 'rsyslog.conf' ]; then
 	scp $co ${target}:/etc/rsyslog.conf
@@ -33,6 +57,7 @@ ssh "${target}" 'pip install tailer'
 
 
 # setting a couple of limits right
+
 ssh "${target}" './prep.sh'
 
 scp "${target}:.rootpasswd" "${target}-rootpasswd"
@@ -47,6 +72,10 @@ ssh "${target}" 'apt-get -y install xinetd telnetd'
 # we need to add this configuration to disable reverse dns lookups
 scp telnet "${target}:/etc/xinetd.d/"
 
+# installing ftp daemon
+ssh "${target}" 'apt-get -y install vsftpd'
+scp vsftpd.conf "${target}:/etc/"
+ssh ${target} '/etc/init.d/vsftpd restart'
 
 # XXX CAREFUL, hardcoded replace
 ssh "${target}" "sed -i 's/^telnet/#telnet/g' /etc/inetd.conf"
